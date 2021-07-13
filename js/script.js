074bdb8b6c64a59ff807c0605810967779bf36fd
pavel <paul702600@gmail.com>
2021-07-13 20:39:34 +0700

    before second check

--- js/script.js
+++ js/script.js
@@ -2,8 +2,15 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     margin: 30,
     dots: true,
-    items: 1,
+    items: 3,
     autoWidth: true,
     loop: true,
+    responsive: {
+      0: {
+        margin: 10,
+        items: 1,
+      },
+      900: {},
+    },
   });
 });
